cda4b3b1fe0e896207ac1a7b89376b26f0bb5f96
JoaoPLF <joaoplfonseca@hotmail.com>
2024-10-24 07:15:00 -0400

    Add native module

---
+++ modules/socket/index.ts
@@ -0,0 +1,25 @@
+import { NativeModulesProxy, EventEmitter, Subscription } from 'expo-modules-core';
+
+// Import the native module. On web, it will be resolved to Socket.web.ts
+// and on native platforms to Socket.ts
+import SocketModule from './src/SocketModule';
+import { ChangeEventPayload } from './src/Socket.types';
+
+// Get the native constant value.
+export const PI = SocketModule.PI;
+
+export function hello(): string {
+  return SocketModule.hello();
+}
+
+export async function setValueAsync(value: string) {
+  return await SocketModule.setValueAsync(value);
+}
+
+const emitter = new EventEmitter(SocketModule ?? NativeModulesProxy.Socket);
+
+export function addChangeListener(listener: (event: ChangeEventPayload) => void): Subscription {
+  return emitter.addListener<ChangeEventPayload>('onChange', listener);
+}
+
+export { ChangeEventPayload };
